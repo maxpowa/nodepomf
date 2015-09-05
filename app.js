4f06c11bc77d97f6ec6dcc37ca36ff1ace27cedf
Max Gurela <maxpowa1@gmail.com>
2015-09-05 02:53:10 -0700

    cleanup

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var upload = require('./routes/upload');
-var potato = require('./routes/potato');
 
 var app = express();
 
@@ -16,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/potato', potato);
 app.use('/upload', upload);
 
 // catch 404 and forward to error handler
--- routes/potato.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('potato', { title: 'Express' });
-});
-
-module.exports = router;
