fd44d707e5c02f8f82966f731df81b344f352b2a
Max Gurela <maxpowa1@gmail.com>
2015-11-10 22:56:53 -0800

    Bugfixes

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM node:4.2.1-onbuild
 
 VOLUME ["/usr/src/app/files", "/usr/src/app/database.db"]
 
+ENV NODE_ENV production
 ENV PORT 80
 EXPOSE 80
--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressSession({ secret: config.SESSION_SECRET || 'some super secret key' }));
+app.use(expressSession({ secret: config.SESSION_SECRET || 'some super secret key', resave: false, saveUninitialized: false }));
 
 if (auth) {
   app.use(passport.initialize());
@@ -101,8 +101,8 @@ app.get('/logout', function(req, res){
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  if (req.url !== '/error') {
-      res.redirect(404, '/error');
+  if (req.url !== '/404') {
+      res.redirect('/404');
   } else {
       var error = new Error('Not found');
       error.status = 404;
